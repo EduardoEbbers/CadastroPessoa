5d42dcf3d126afa78bfbc698f43e174aa4450d6d
Carlos Ebbers <cel.ebbers@gmail.com>
2021-09-15 17:10:41 -0300

    Criacao da camada de HttpException

---
+++ CadatroPessoaWebApi/Exceptions/HttpException.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadatroPessoaWebApi.Exceptions
+{
+    public class HttpException : HttpResponseException, IHttpException
+    {
+        public HttpException(string message, HttpStatusCode statusCode) :
+            base(new HttpResponseMessage()
+            {
+                Content = new StringContent(message),
+                StatusCode = statusCode
+            })
+        {
+
+        }
+
+        public ObjectResult HttpMessageException()
+        {
+            ObjectResult objResult = null;
+
+            switch ((int)Response.StatusCode)
+            {
+                case 400:
+                    objResult = new BadRequestObjectResult(Response.Content.ReadAsStringAsync().Result);
+                    break;
+                case 404:
+                    objResult = new NotFoundObjectResult(Response.Content.ReadAsStringAsync().Result);
+                    break;
+            }
+            return objResult;
+        }
+    }
+}
